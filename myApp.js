cd951e95b067d3bfb29a2cd2c8a23f9297ee1844
AsaMitaka <cepelits@gmail.com>
2024-02-14 01:16:57 +0200

    + createManyPeople

--- myApp.js
+++ myApp.js
@@ -18,15 +18,22 @@ const createAndSavePerson = (done) => {
   });
 
   person.save(function (err, data) {
-    if (err) {
-      console.log(err);
-    }
+    if (err) return console.log(err);
     done(null, data);
   });
 };
 
+const arrayOfPeople = [
+  { name: 'Frankie', age: 74, favoriteFoods: ['Del Taco'] },
+  { name: 'Sol', age: 76, favoriteFoods: ['roast chicken'] },
+  { name: 'Robert', age: 78, favoriteFoods: ['wine'] },
+];
+
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
